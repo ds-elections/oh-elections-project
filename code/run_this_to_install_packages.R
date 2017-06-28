e9bc38be7f3c7ca956430953063c32c872a2a8eb
Matthew Yancheff <yanchefm@reed.edu>
2017-06-28 14:29:02 -0700

    last few things

---
+++ code/run_this_to_install_packages.R
@@ -0,0 +1,8 @@
+
+install.packages("readr")
+install.packages("tidyverse")
+install.packages("mdsr")
+install.packages("lubridate")
+
+#sharable link to ohio df:
+#https://drive.google.com/open?id=0B16R3n7VKQteeEh5SmozTXRVb2s
